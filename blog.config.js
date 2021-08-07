d2e989cee36872b9e58b604ba518d43ff83671ac
TaoLiu2814493753 <44283308+TaoLiuHK@users.noreply.github.com>
2021-08-07 23:04:15 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,9 +1,9 @@
 const BLOG = {
-  title: 'CRAIGARY',
-  author: 'Craig Hart',
-  email: 'i@craigary.net',
-  link: 'https://nobelium.vercel.app',
-  description: 'This gonna be an awesome website.',
+  title: 'CoffCodes',
+  author: 'CoffCode',
+  email: 'CoffCods@gmail.com',
+  link: 'https://coffcodes.notion.site/coffcodes/BigData-a7302402a5d54f8794d03983fb201d7a',
+  description: 'awesome.',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   appearance: 'auto', // ['light', 'dark', 'auto'],
   font: 'sans-serif', // ['sans-serif', 'serif']
